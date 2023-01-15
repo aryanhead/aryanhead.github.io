05647629ebd4eb69dcd8f2d9321eeff13e93e007
aryanhead <122706956+aryanhead@users.noreply.github.com>
2023-01-15 12:07:37 +1100

    Update game.js

--- game.js
+++ game.js
@@ -21,4 +21,47 @@ function draw() {
   requestAnimationFrame(draw);
 }
 
+let blocks = [
+  { x: 100, y: 100, type: "dirt" },
+  { x: 150, y: 150, type: "stone" },
+  { x: 200, y: 200, type: "wood" }
+];
+
+let inventory = [];
+
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillRect(player.x, player.y, 50, 50);
+
+  blocks.forEach(block => {
+    if (block.type === "dirt") {
+      ctx.fillStyle = "brown";
+    } else if (block.type === "stone") {
+      ctx.fillStyle = "gray";
+    } else if (block.type === "wood") {
+      ctx.fillStyle = "brown";
+    }
+    ctx.fillRect(block.x, block.y, 50, 50);
+  });
+
+  requestAnimationFrame(draw);
+}
+
+document.onmousedown = function(e) {
+  let mouseX = e.clientX;
+  let mouseY = e.clientY;
+
+  blocks.forEach((block, index) => {
+    if (
+      mouseX > block.x &&
+      mouseX < block.x + 50 &&
+      mouseY > block.y &&
+      mouseY < block.y + 50
+    ) {
+      inventory.push(block.type);
+      blocks.splice(index, 1);
+    }
+  });
+};
+
 draw();
